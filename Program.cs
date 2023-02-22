d46cd5ef2ac0451587326210fb19c32d989b425b
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-22 19:24:56 +0500

    add dz8

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+System.Console.WriteLine("Imput number of 5 length");
+string? number = Console.ReadLine();
+
+if (number!.Length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        System.Console.WriteLine("yes");
+    }
+    else
+    {
+        System.Console.WriteLine("No");
+    }
+}
+else
+{
+    System.Console.WriteLine("wrong");
+}
